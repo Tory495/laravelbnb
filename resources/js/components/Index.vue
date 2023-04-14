fb59c51af33e4dd16eae0a61e07dd62fb14b014e
Tory495 <ivarthewebdeveloper@gmail.com>
2023-04-14 19:03:53 +0300

    test commit

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,11 +1,5 @@
 <template>
-<<<<<<< HEAD
 	<h1>index vue component</h1>
-=======
-    <div class="col-md-12">
-        <h1 class="h1">Hello vue 123</h1>
-    </div>
->>>>>>> refs/remotes/origin/main
 </template>
 
 <script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {createRouter, createWebHashHistory} from 'vue-router';
 import Index from './components/Index';
 
@@ -9,18 +8,6 @@ const routes = [
 const router = createRouter({
 	history: createWebHashHistory,
 	routes
-=======
-import Index from './components/Index';
-import VueRouter from 'vue-router';
-
-const routes = [
-    { path:'/', component: Index}
-]
-
-const router = new VueRouter({
-    routes,
-    mode: "history"
->>>>>>> refs/remotes/origin/main
 });
 
 export default router;
