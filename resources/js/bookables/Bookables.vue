351184ebadf38fb94b48878e31344ce506ad65bc
Tory495 <ivarthewebdeveloper@gmail.com>
2023-04-27 20:23:29 +0300

    192. Child components (using components in other components)

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <bookables-list-item></bookables-list-item>
+    <bookables-list-item></bookables-list-item>
+    <bookables-list-item></bookables-list-item>
+  </div>
+</template>
+
+<script>
+import BookablesListItem from "./BookablesListItem";
+
+export default {
+  components: {
+    BookablesListItem,
+  },
+};
+</script>
+
+<style>
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,7 @@ import Index from './components/Index';
 const routes = [
 	{ name: 'home', path: '/home', component: require('./components/Home').default },
 	{ name: 'example', path: '/example', component: require('./components/Example').default },
+	{ name: 'bookables', path: '/bookables', component: require('./bookables/Bookables').default }
 ];
 
 const router = new VueRouter({
