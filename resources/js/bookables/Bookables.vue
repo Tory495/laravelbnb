184ee3875cdf73f01783d375d33915de71e4ec87
Tory495 <ivarthewebdeveloper@gmail.com>
2023-05-05 19:07:19 +0300

    198. Conditional rendering (v-if)

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="container">
     <bookables-list-item
+      v-if="bookable1"
       :item-title="bookable1.title"
       :item-content="bookable1.content"
       :item-price="1000"
     ></bookables-list-item>
     <bookables-list-item
+      v-if="bookable2"
       :item-title="bookable2.title"
       :item-content="bookable2.content"
-      :item-price="1500"
-    ></bookables-list-item>
-    <bookables-list-item
-      :item-title="bookable3.title"
-      :item-content="bookable3.content"
       :item-price="1500"
     ></bookables-list-item>
   </div>
@@ -27,14 +24,8 @@ export default {
   },
   data() {
     return {
-      bookable1: {
-        title: "very cheap villa",
-        content: "very cheap villa you know",
-      },
-      bookable2: {
-        title: "cheap villa",
-        content: "cheap villa we know",
-      },
+      bookable1: null,
+      bookable2: null
     };
   },
   created() {
@@ -50,21 +41,7 @@ export default {
         title: "cheap villa",
         content: "cheap villa we know",
       };
-      this.bookable3 = {
-        title: "bookable 3",
-        content: "bookable 3 villa we know",
-      };
     }, 5000);
-
-    setTimeout(() => {
-      console.log("second");
-      this.bookable2.title = "bookable 2 title you see";
-    }, 8000);
-
-    setTimeout(() => {
-      console.log("third");
-      this.bookable3.title = "u ll'nt see this";
-    }, 12000);
   },
 };
 </script>
