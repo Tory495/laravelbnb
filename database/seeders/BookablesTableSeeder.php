a9d20044f9aaeae95bf798c8ff5ae6009aebebd5
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-09 00:37:40 +0300

    205. Model factory, database seeder and faker

--- database/seeders/BookablesTableSeeder.php
+++ database/seeders/BookablesTableSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bookable;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookablesTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(BookablesTableSeeder::class);
+        Bookable::factory()->count(100)->create();
     }
 }
