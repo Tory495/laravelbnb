50464b9b69014ddc291f090d685042e6d9770968
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-09 17:54:22 +0300

    220. Availability component (horizontal forms, scoped styles).

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h6 class="text-uppercase text-secondary text-weight-bolder">
+      check availability
+    </h6>
+    <div class="row mb-2">
+      <div class="form-group col-md-6">
+        <label for="from">From</label>
+        <input
+          type="text"
+          name="for"
+          class="form-control form-control-sm"
+          placeholder="Start date"
+        />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="to">To</label>
+        <input
+          type="text"
+          name="to"
+          class="form-control form-control-sm"
+          placeholder="End date"
+        />
+      </div>
+    </div>
+    <div class="d-grid gap">
+        <button class="btn btn-secondary btn-block">Check!</button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+label {
+  font-size: 0.7rem;
+  text-transform: uppercase;
+  color: gray;
+  font-weight: bolder;
+}
+</style>
