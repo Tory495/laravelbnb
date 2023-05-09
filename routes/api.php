93d81d80890b253321e68b1f81c812a226181e88
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-09 14:25:52 +0300

    217. Laravel controllers.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Bookable;
+use App\Http\Controllers\Api\BookableController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', function (Request $request) {
-    return Bookable::all();
-});
-
-Route::get('bookables/{id}', function (Request $request, $id) {
-    return Bookable::findOrFail($id);
-});
+Route::get('bookables', [BookableController::class, 'index']);
+Route::get('bookables/{id}', [BookableController::class, 'show']);
