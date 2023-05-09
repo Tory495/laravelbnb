2e29f31c1f3fa57cdae414cd2f861537966d91f8
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-09 14:02:03 +0300

    216. Understanding CSS grid system.

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,23 +1,38 @@
 <template>
-    <div v-if="bookable">
-        {{ bookable.title }}
+  <div class="row">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="loading">data is loading...</div>
+          <div v-else>
+            <h5>{{ bookable.title }}</h5>
+            <hr />
+            <article>{{ bookable.description }}</article>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="col-md-4">availability & prices</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            bookable: null
-        }
-    },
-    
-    created() {
-        axios.get(`/api/bookables/${this.$route.params.id}`).then(response => this.bookable = response.data);
-    }
-}
+  data() {
+    return {
+      bookable: null,
+      loading: true,
+    };
+  },
+
+  created() {
+    axios.get(`/api/bookables/${this.$route.params.id}`).then((response) => {
+      this.bookable = response.data;
+      this.loading = false;
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
