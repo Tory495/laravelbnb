283b36b2fc695bc171476e413208b897dc33408a
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-09 18:01:16 +0300

    221. Form input binding (v-model).

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -11,6 +11,7 @@
           name="for"
           class="form-control form-control-sm"
           placeholder="Start date"
+          v-model="from"
         />
       </div>
       <div class="form-group col-md-6">
@@ -20,18 +21,25 @@
           name="to"
           class="form-control form-control-sm"
           placeholder="End date"
+          v-model="to"
         />
       </div>
     </div>
     <div class="d-grid gap">
-        <button class="btn btn-secondary btn-block">Check!</button>
+      <button class="btn btn-secondary btn-block">Check!</button>
     </div>
-    
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      from: null,
+      to: null,
+    };
+  },
+};
 </script>
 
 <style scoped>
