baa4cfbb719d4ffac7800e10eb3ca30d5500ace2
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-10 23:37:07 +0300

    236. Review model factory & seeder.

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id' => Str::uuid(),
+            'content' => $this->faker->sentences(5, true),
+            'rating' => random_int(1, 5)
+        ];
+    }
+}
