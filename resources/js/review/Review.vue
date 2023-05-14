8739306907ec372245706b36e24237abb00af3d0
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-14 16:30:20 +0300

    244. Review Page component markup and route.

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="form-group mb-4">
+        <label class="text-muted">Select rating (1 is worst | 5 is best)</label>
+        <star-rating :rating="5" class="fa-3x"></star-rating>
+    </div>
+    <div class="form-group mb-4">
+        <label for="content" class="text-muted">Write your experience with</label>
+        <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-lg btn-primary w-100">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,16 +1,12 @@
 import VueRouter from 'vue-router';
-import Index from './components/Index';
 
-const routes = [
-	{ name: 'home', path: '/home', component: require('./components/Home').default },
-	{ name: 'example', path: '/example', component: require('./components/Example').default },
-	{ name: 'bookables', path: '/bookables', component: require('./bookables/Bookables').default },
-	{ name: 'bookable', path: '/bookable/:id', component: require('./bookable/Bookable').default }
-];
-
-const router = new VueRouter({
+export default new VueRouter({
 	mode: "history",
-	routes
+	routes: [
+		{ name: 'home', path: '/home', component: require('./components/Home').default },
+		{ name: 'example', path: '/example', component: require('./components/Example').default },
+		{ name: 'bookables', path: '/bookables', component: require('./bookables/Bookables').default },
+		{ name: 'bookable', path: '/bookable/:id', component: require('./bookable/Bookable').default },
+		{ name: 'review', path: '/review/:id', component: require('./review/Review').default }
+	]
 });
-
-export default router;
