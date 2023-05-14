6de828d6c30e5d73c1ec499e2c529e9dfa31204c
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-05-14 15:07:37 +0300

    242. Star Rating component logic (calculating stars to display from average rating).

--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="d-flex">
-    <i class="fas fa-star" v-for="star in fullStars" :key="`fullStar${star}`"></i>
+    <i
+      class="fas fa-star"
+      v-for="star in fullStars"
+      :key="`fullStar${star}`"
+    ></i>
     <i class="fas fa-star-half-alt" v-if="halfStar"></i>
-    <i class="far fa-star" v-for="star in emptyStars" :key="`emptyStar${star}`"></i>
+    <i
+      class="far fa-star"
+      v-for="star in emptyStars"
+      :key="`emptyStar${star}`"
+    ></i>
   </div>
 </template>
 
@@ -11,13 +19,14 @@ export default {
   props: { rating: Number },
   computed: {
     fullStars() {
-      return 3;
+      return Math.round(this.rating);
     },
     halfStar() {
-      return true;
+      const fraction = Math.round((this.rating - Math.floor(this.rating)) * 10);
+      return fraction > 0 && fraction < 5;
     },
     emptyStars() {
-      return 1;
+      return 5 - Math.ceil(this.rating);
     },
   },
 };
