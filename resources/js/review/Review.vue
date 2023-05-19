e213655b2a932cc953446e772276b3c618fa5638
Tory495 <ivarthewebdeveloper@gmail.com>
2023-05-19 19:03:41 +0300

    250. Checking if review already exists on frontend.

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,35 +1,68 @@
 <template>
   <div>
-    <div class="form-group mb-4">
-        <label class="text-muted">Select rating (1 is worst | 5 is best)</label>
-        <star-rating v-model="review.rating" class="fa-3x"></star-rating>
+    <div v-if="loading">Data is loading...</div>
+    <div v-else>
+      <div v-if="alreadyReviewed">You already reviewed this booking</div>
+      <div v-else>
+        <div class="form-group mb-4">
+          <label class="text-muted"
+            >Select rating (1 is worst | 5 is best)</label
+          >
+          <star-rating v-model="review.rating" class="fa-3x"></star-rating>
+        </div>
+        <div class="form-group mb-4">
+          <label for="content" class="text-muted"
+            >Write your experience with</label
+          >
+          <textarea
+            v-model="review.content"
+            name="content"
+            cols="30"
+            rows="10"
+            class="form-control"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-lg btn-primary w-100">
+          Submit
+        </button>
+      </div>
     </div>
-    <div class="form-group mb-4">
-        <label for="content" class="text-muted">Write your experience with</label>
-        <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
-    </div>
-    <button type="submit" class="btn btn-lg btn-primary w-100">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            review: {
-                rating: 5,
-                content: null
-            }
-        }
+  data() {
+    return {
+      review: {
+        rating: 5,
+        content: null,
+      },
+      existingReview: null,
+      loading: null,
+    };
+  },
+  created() {
+    this.loading = true;
+    // 1. If review already exists (in reviews table by id)
+
+    axios
+      .get(`/api/reviews/${this.$route.params.id}`)
+      .then((response) => {
+        this.existingReview = response.data.data;
+      })
+      .catch((err) => {})
+      .finally(() => (this.loading = false));
+    // 2. Fetch a booking by a review key
+    // 3. Store the review
+  },
+  computed: {
+    alreadyReviewed() {
+      return this.existingReview !== null;
     },
-    created() {
-        // 1. If review already exists (in reviews table by id)
-        // 2. Fetch a booking by a review key
-        // 3. Store the review
-    },
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
