49a6c57c76667192cde2304bc876b2df0b8103ab
Tory495 <ivarthewebdeveloper@gmail.com>
2023-05-19 18:24:53 +0300

    247. v-model explained, adding v-model support to custom components.

--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -4,36 +4,31 @@
       class="fas fa-star"
       v-for="star in fullStars"
       :key="`fullStar${star}`"
-      @click="$emit('rating:changed', star)"
+      @click="$emit('input', star)"
     ></i>
     <i class="fas fa-star-half-alt" v-if="halfStar"></i>
     <i
       class="far fa-star"
       v-for="star in emptyStars"
       :key="`emptyStar${star}`"
-      @click="$emit('rating:changed', fullStars + star)"
+      @click="$emit('input', fullStars + star)"
     ></i>
   </div>
 </template>
 
 <script>
 export default {
-  props: { rating: Number },
+  props: { value: Number },
   computed: {
     fullStars() {
-      return Math.round(this.rating);
+      return Math.round(this.value);
     },
     halfStar() {
-      const fraction = Math.round((this.rating - Math.floor(this.rating)) * 10);
+      const fraction = Math.round((this.value - Math.floor(this.value)) * 10);
       return fraction > 0 && fraction < 5;
     },
     emptyStars() {
-      return 5 - Math.ceil(this.rating);
-    },
-  },
-  methods: {
-    heyBuddy(smt) {
-      console.log(`hey buddy im here ${smt}`);
+      return 5 - Math.ceil(this.value);
     },
   },
 };
