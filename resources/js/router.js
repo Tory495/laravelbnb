18dfebf6bea0122367f5c687e7855be8452939a7
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-06-11 17:10:18 +0300

    268. Fixing application Vue issues.

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,8 +3,6 @@ import VueRouter from 'vue-router';
 export default new VueRouter({
 	mode: "history",
 	routes: [
-		{ name: 'home', path: '/home', component: require('./components/Home').default },
-		{ name: 'example', path: '/example', component: require('./components/Example').default },
 		{ name: 'bookables', path: '/bookables', component: require('./bookables/Bookables').default },
 		{ name: 'bookable', path: '/bookable/:id', component: require('./bookable/Bookable').default },
 		{ name: 'review', path: '/review/:id', component: require('./review/Review').default }
