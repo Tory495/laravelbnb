ae12dc248bff67eaca8843ce4695b36bf6d87907
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-06-11 23:38:02 +0300

    270. Setting up Vuex.

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,7 @@ import StarRating from "./shared/components/StarRating";
 import FatalError from "./shared/components/FatalError";
 import Success from "./shared/components/Success";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import storeDefinition from "./store";
 
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
@@ -26,9 +27,12 @@ Vue.component("fatal-error", FatalError);
 Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors)
 
+const store = new Vuex.Store(storeDefinition);
+
 let app = new Vue({
 	el: '#app',
 	router,
+	store,
 	components: {
 		Index,
 	}
