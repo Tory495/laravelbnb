80eca95606849e7842e5bb7c6005808fd2a3324f
Tory495 <ivarthewebdeveloper@gmail.com>
2023-06-23 20:03:34 +0300

    289. List transitions - animating adding/deleting to a list (v-for).

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -11,29 +11,30 @@
           <span v-else>Empty</span>
         </h6>
       </div>
-      <div v-for="item in basket" :key="item.bookable.id">
-        <div class="border-top pt-2 pb-2 d-flex justify-content-between">
-          <router-link
-            :to="{ name: 'bookable', params: { id: item.bookable.id } }"
-          >
-            {{ item.bookable.title }}
-          </router-link>
-          <span class="font-weight-bold">${{ item.price.total }}</span>
+      <transition-group name="fade" tag="div">
+        <div v-for="item in basket" :key="item.bookable.id">
+          <div class="border-top pt-2 pb-2 d-flex justify-content-between">
+            <router-link
+              :to="{ name: 'bookable', params: { id: item.bookable.id } }"
+            >
+              {{ item.bookable.title }}
+            </router-link>
+            <span class="font-weight-bold">${{ item.price.total }}</span>
+          </div>
+          <div class="pb-2 pb-2 d-flex justify-content-between">
+            <span> From {{ item.dates.from }} </span>
+            <span> To {{ item.dates.to }} </span>
+          </div>
+          <div class="pt-2 pb-2 d-flex justify-content-end">
+            <button
+              class="btn btn-sm btn-outline-secondary"
+              @click="$store.dispatch('removeFromBasket', item.bookable.id)"
+            >
+              <i class="fas fa-trash-alt"></i>
+            </button>
+          </div>
         </div>
-        <div class="pb-2 pb-2 d-flex justify-content-between">
-          <span>
-            From {{ item.dates.from }}
-          </span>
-          <span>
-            To {{ item.dates.to }}
-          </span>
-        </div>
-        <div class="pt-2 pb-2 d-flex justify-content-end">
-          <button class="btn btn-sm btn-outline-secondary" @click="$store.dispatch('removeFromBasket', item.bookable.id)">
-            <i class="fas fa-trash-alt"></i>
-          </button>
-        </div>
-      </div>
+      </transition-group>
     </div>
   </div>
 </template>
