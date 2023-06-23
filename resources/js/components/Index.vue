ff92fa6e8b9a0bc4af56220c50cb3fb3bbd050ce
Tory495 <ivarthewebdeveloper@gmail.com>
2023-06-23 17:22:18 +0300

    283. Vuex getters - how many items in the basket.

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -4,9 +4,20 @@
       <router-link class="navbar-brand mr-auto" :to="{ name: 'bookables' }"
         >Bookables</router-link
       >
+      <router-link class="btn nav-button" :to="{}">
+        Basket:
+        <span v-if="itemsInBasket">{{ itemsInBasket }}</span>
+      </router-link>
     </nav>
     <div class="container-md mt-5 mb-5 pr-5 pl-5">
       <router-view></router-view>
     </div>
   </div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: mapGetters(["itemsInBasket"]),
+};
+</script>
